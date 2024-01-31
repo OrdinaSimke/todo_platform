89dea37332f8816f8c9c7f1fc882b4b9043cf2cc
simke <simke.nys@gmail.com>
2024-01-31 09:59:15 +0100

    test

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log('clerk event data', evt.data);
+  alert(JSON.stringify(evt.data));
 
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } =

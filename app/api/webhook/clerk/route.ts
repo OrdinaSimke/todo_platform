2e0ee77b1f95bf2746a4dd34f03cc2193cd0cb14
simke <simke.nys@gmail.com>
2024-01-31 15:38:01 +0100

    fix

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,43 +56,22 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === 'user.created') {
-    // const { id, email_addresses, image_url, first_name, last_name, username } =
-    // evt.data;
-
-    // const user = {
-    //   clerkId: id,
-    //   email: email_addresses[0].email_address,
-    //   username: username!,
-    //   firstName: first_name,
-    //   lastName: last_name,
-    //   photo: image_url,
-    // };
-
-    // const newUser = await createUser(user);
-
-    // if (newUser) {
-    //   await clerkClient.users.updateUserMetadata(id, {
-    //     publicMetadata: {
-    //       userId: newUser._id,
-    //     },
-    //   });
-    // }
-
-    // return NextResponse.json({ message: 'OK', user: newUser });
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
 
     const user = {
-      clerkId: '1',
-      email: 's.n@gmail.com',
-      username: 'simkenys'!,
-      firstName: 'sim',
-      lastName: 'ms',
-      photo: '',
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     };
 
     const newUser = await createUser(user);
 
     if (newUser) {
-      await clerkClient.users.updateUserMetadata('1', {
+      await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },

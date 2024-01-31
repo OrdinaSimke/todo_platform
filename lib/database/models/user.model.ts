f2f1c65f034a50fa11c3007d4caaa8db1c94e182
simke <simke.nys@gmail.com>
2024-01-31 15:44:03 +0100

    test

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -2,11 +2,11 @@ import { Schema, model, models } from 'mongoose';
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  photo: { type: String, required: true },
+  email: { type: String, required: false, unique: true },
+  username: { type: String, required: false, unique: true },
+  firstname: { type: String, required: false },
+  lastname: { type: String, required: false },
+  photo: { type: String, required: false },
 });
 
 const User = models.User || model('User', UserSchema);
